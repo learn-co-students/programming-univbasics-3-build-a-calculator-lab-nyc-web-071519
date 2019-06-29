53ad3469129ab1244260c6d3707d13aa67c983a7
Alyssa Farina <alyssafarina695@gmail.com>
2019-06-29 16:52:26 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,24 @@
-def addition(num1, num2)
+def addition(num1 = 5 , num2 = 4)
+  num1 + num 2 
+end
+
+def subtraction(num1c = 10 , num2 = 5)
+  num2 - num 1
+end
+
+def division(num1 = 50 , num2 = 2)
+  num1 / num2
+end
+
+def multiplication(num1 = 4, num2 = 30)
+  num 1 * num2
 
 end
 
-def subtraction(num1, num2)
-
+def modulo(num1 = 34, num2 = 5)
+  num1 % num2
 end
 
-def division(num1, num2)
-
-end
-
-def multiplication(num1, num2)
-
-end
-
-def modulo(num1, num2)
-
-end
-
-def square_root(num)
-
+def square_root(num = 81)
+  Math.sqrt = num
 end
